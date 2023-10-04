56f36a3cde8f384fe5c628d490481daf248e2c62
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2023-10-04 11:00:03 -0300

    Update rar.py

--- files/rar.py
+++ files/rar.py
@@ -1,42 +1,44 @@
 import rarfile
-import os,sys
+import os
+import sys
 from tqdm import tqdm
 
-
-def testingPWD(fn,password):
+def testing_password(fn, password):
     try:
         rar = rarfile.RarFile(fn)
         try:
-           rar.extractall(pwd=password)
-           return 1
-        except:
+            rar.extractall(pwd=password)
+            return 1
+        except rarfile.BadRarFile:
             return -1
-    except:
+    except FileNotFoundError:
         return 0
 
-wordlist = input("Enter your wordlist: ")
-protected_file = input("Enter your RAR protected file: ")
+def main():
+    wordlist = input("Enter your wordlist: ")
+    protected_file = input("Enter your RAR protected file: ")
 
-passwords = [line.strip() for line in open(wordlist)]
+    if not os.path.isfile(protected_file):
+        print(f"\033[0;31m[+]\033[0m File Not Found {protected_file}")
+        sys.exit(1)
 
-if not os.path.isfile(protected_file):
-    print(f"\033[0;31m[+]\033[0m File Not Found {protected_file}")
-    sys.exit()
+    if not os.path.isfile(wordlist):
+        print(f"\033[0;31m[+]\033[0m Wordlist Not Found: {wordlist}")
+        sys.exit(1)
 
-if not os.path.isfile(wordlist):
-    print(f"\033[0;31m[+]\033[0m Wordlist Not Found: {wordlist}")
-    sys.exit()
+    print("")
+    
+    with open(wordlist, "r") as file:
+        passwords = [line.strip() for line in file]
 
-print("")
+    for password in tqdm(passwords, desc="\033[0;31m[+]\033[0m Trying Password..."):
+        stripped_password = password.replace('\r\n', '').replace('\n', '')
+        result = testing_password(protected_file, stripped_password)
+        if result == 1:
+            print(f"\033[0;32m[!]\033[0m Password Found: {stripped_password}")
+            sys.exit(0)
+        elif result == -1:
+            print(f"\033[0;31m[+]\033[0m Not Found: {stripped_password}")
 
-file = open(wordlist,"r")
-for password in tqdm(passwords, desc="\033[0;31m[+]\033[0m Trying Password..."):
-    replace=password.strip().replace('\r\n','').replace('\n','')
-    strip = testingPWD(protected_file,replace)
-    if strip ==1:
-        print(f"\033[0;32m[!]\033[0m Password Found: {replace}")
-        sys.exit()
-    elif strip ==-1:
-        print(f"\033[0;31m[+]\033[0m Not Found: {replace}")
-    else:
-        sys.exit()
+if __name__ == "__main__":
+    main()
