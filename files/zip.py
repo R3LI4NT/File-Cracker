77f34a8a7701fdd5df40ea42d371cffe34a43d85
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2023-10-04 10:52:39 -0300

    Update zip.py

--- files/zip.py
+++ files/zip.py
@@ -1,7 +1,6 @@
 from tqdm import tqdm
 import zipfile
 
-
 wordlist = input("Enter your wordlist: ")
 protected_file = input("Enter your ZIP protected file: ")
 
@@ -9,21 +8,22 @@ try:
     zip_file = zipfile.ZipFile(protected_file)
     number_pass = len(list(open(wordlist, "rb")))
 
-except:
+except FileNotFoundError:
     print("\n\033[0;31mFile or wordlist not found, exiting...\033[0m")
     exit(0)
 
 print("\n")
 
-with open(wordlist, "rb") as wordlist:
-    for word in tqdm(wordlist,desc="\033[0;31m[+]\033[0m Trying Password...", total=number_pass, unit='word'):
+with open(wordlist, "rb") as wordlist_file:
+    for word in tqdm(wordlist_file, desc="\033[0;31m[+]\033[0m Trying Password...", total=number_pass, unit='word'):
         try:
             zip_file.extractall(pwd=word.strip())
 
-        except:
+        except Exception as e:
             continue
 
         else:
             print("\033[0;34m[!]\033[0m Password Found:", word.decode().strip())
             print('Exiting...')
-            exit(0)             
+            exit(0)
+   
